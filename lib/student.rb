df2661145ef2d2aeb62c8fd9363f63409d70779c
Travis Lee <travisslee223@gmail.com>
2020-10-09 16:52:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,14 @@ class Student
   end
   
   def save
-    
+    sql = <<-SQL
+      INSERT INTO students (name, grade) 
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.id, self.name, self.grade)
+ 
+  end
   end
   
 end
